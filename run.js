fa81c89a065d936be60d43b31fb216aed286e0ba
Alexander Bazo <alexanderbazo@googlemail.com>
2016-08-29 16:07:43 +0200

    Adding start routine for windows and mac

--- run.js
+++ run.js
@@ -1,30 +1,61 @@
 /* eslint-env node */
 
-var shell = require("shelljs");
+var shell = require("shelljs"),
+  os = require("os");
 (function() {
   "use strict";
   var that = {};
 
-  function start() {
-
-  }
-
-  function checkForUpdates() {
-    var result = shell.exec("git remote -v  update");
-    console.log(result);
+  function getOperatingSystemType() {
+    return os.type();
   }
 
   function update() {
-
+    var result = shell.exec("git remote -v  update");
+    if (result.stderr.indexOf("[up to date]") === -1) {
+      return true;
+    }
+    return false;
   }
 
   function build() {
+    shell.exec("grunt srv && grunt www");
+  }
+
+  function startServer(callback) {
+    shell.exec("cd build && npm start &", function(code, stdoutm, stderr) {
+      if (callback) {
+        callback();
+      }
+    });
+  }
+
+  function startClient() {
+    var os = getOperatingSystemType();
+    switch (os) {
+      case "Darwin":
+        shell.exec("open -a 'Google Chrome' http://localhost:8888");
+        break;
+      case "Widows_NT":
+        shell.exec("start chrome --kiosk http://localhost:8888");
+        break;
+      default:
+        console.log("Could not start client automattically. Open http://localhost:8888 in your browser.");
+        break;
+    }
+  }
+
+  function onServerClosed() {
 
   }
 
   function run() {
-    console.log("Starting SolarBoard");
-    checkForUpdates();
+    let wasUpdated = update();
+    if (wasUpdated) {
+      build();
+    }
+    startServer(onServerClosed);
+    setTimeout(startClient, 5000);
   }
 
   that.run = run;
