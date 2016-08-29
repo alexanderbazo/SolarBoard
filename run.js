63d470901c49ec2621c1e6ba6c9c3eb8b4130c09
Alexander Bazo <alexanderbazo@googlemail.com>
2016-08-29 16:17:13 +0200

    Adding start routine for windows and mac

--- run.js
+++ run.js
@@ -11,6 +11,7 @@ var shell = require("shelljs"),
   }
 
   function update() {
+    console.log("Checking for updates");
     var result = shell.exec("git remote -v  update");
     if (result.stderr.indexOf("[up to date]") === -1) {
       return true;
@@ -19,10 +20,12 @@ var shell = require("shelljs"),
   }
 
   function build() {
+    console.log("Building server and client");
     shell.exec("grunt srv && grunt www");
   }
 
   function startServer(callback) {
+    console.log("Starting server");
     shell.exec("cd build && npm start &", function(code, stdoutm, stderr) {
       if (callback) {
         callback();
@@ -31,6 +34,7 @@ var shell = require("shelljs"),
   }
 
   function startClient() {
+    console.log("Starting client");
     var os = getOperatingSystemType();
     switch (os) {
       case "Darwin":
